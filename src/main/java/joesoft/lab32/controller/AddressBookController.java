59611b44782979d66daee897eeb1431212395748
Joe Frederick Samuel <me@JoeFS.com>
2020-02-09 20:50:13 -0500

    General CORS

--- src/main/java/joesoft/lab32/controller/AddressBookController.java
+++ src/main/java/joesoft/lab32/controller/AddressBookController.java
@@ -113,7 +113,7 @@ public class AddressBookController {
      * API for viewing AddressBook object
      * @return JSON of AddressBook object
      */
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin
     @GetMapping("/api/viewbook")
     @ResponseBody
     public AddressBook viewBookAPI(){
